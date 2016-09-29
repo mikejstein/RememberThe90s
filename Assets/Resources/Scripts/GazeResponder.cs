6864547d11c3f8386edc77a959a838517d58b5ea
Mike Stein <mike.j.stein@gmail.com>
2016-09-29 11:28:15 -0700

    Initial Commit

---
+++ Assets/Resources/Scripts/GazeResponder.cs
@@ -0,0 +1,61 @@
+﻿// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class GazeResponder : MonoBehaviour, IGvrGazeResponder {
+	private Vector3 startingPosition;
+
+	void Start() {
+		startingPosition = transform.localPosition;
+		SetGazedAt(false);
+	}
+
+	void LateUpdate() {
+		GvrViewer.Instance.UpdateState();
+		if (GvrViewer.Instance.BackButtonPressed) {
+			Application.Quit();
+		}
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		Debug.Log("FUCKING GAZED AT");
+		//GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.white;
+	}
+
+
+
+	#region IGvrGazeResponder implementation
+
+	/// Called when the user is looking on a GameObject with this script,
+	/// as long as it is set to an appropriate layer (see GvrGaze).
+	public void OnGazeEnter() {
+		Debug.Log("ENTERING GAZE");
+		SetGazedAt(true);
+	}
+
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit() {
+		Debug.Log("EXITING GAZE");
+		SetGazedAt(false);
+	}
+
+	public void OnGazeTrigger() {
+		Debug.Log("TRIGGERED");
+	}
+	#endregion
+}
---
+++ Assets/Resources/Scripts/Hexagon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+
+public enum ColorStates {
+	Closed,
+	Open,
+	Gazed
+}
+
+
+public class Hexagon : MonoBehaviour {
+	private GameObject[] insideCubes;
+	// Use this for initialization
+	void Start () {
+		//First, get children
+		Transform[] huh = gameObject.GetComponentsInChildren<Transform>();
+		Debug.Log(huh.Length);
+		//insideCubes = insideCubes.Where(child => child.tag == "InsideHex").ToArray;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}	
+
+}
